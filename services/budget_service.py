f07221958156593d25662125e3b9e8ffc0e6292d
Admiral-Ham <ai.does.math152@gmail.com>
2025-12-02 22:03:53 -0800

    Created get_budget_by_user_id and get_budget_by_id.

--- services/budget_service.py
+++ services/budget_service.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 from config.db import db
+from schemas.budget_schema import Budget
+from bson import ObjectId
+from schemas.objectID_helper import str_to_object_id
 
 default_categories = ["Needs", "Saves", "Wants"] #business logic
 
-def create_budget(user_id, total_amount, name : str, first_time ):
+def create_budget(user_id: str, total_amount: float, name : str):
     existing_budget = db.budgets.find_one({"user_id": user_id})
     if existing_budget:
         raise ValueError("Budget already exists for this user")
@@ -15,9 +18,33 @@ def create_budget(user_id, total_amount, name : str, first_time ):
         "categories": [],
         "created_on": datetime.utcnow(),
     }
-
+    valid_budget_doc = Budget.model_validate(budget_document)
+    mongo_budget_doc = valid_budget_doc.model_dump(by_alias=True, exclude_none=True)
+    mongo_budget_doc["user_id"] = str_to_object_id(valid_budget_doc["user_id"])
     #if len(budget_document["categories"]) < 1:
     #    raise ValueError("Budget must have at least one category"
     db.budgets.insert_one(budget_document)
     return {"message": "Budget created successfully"}
 
+def get_budget_by_user_id(user_id: str) -> dict:
+    """
+    Docstring for get_budget_by_user_id
+    
+    :param user_id: Description
+    :type user_id: str
+    :return: Description
+    :rtype: dict
+    """
+    user_id_oID = str_to_object_id(user_id)
+    return db.budget.find_one(user_id_oID)
+
+def get_budget_by_id(budget_id: ObjectId) -> dict:
+    """
+    Docstring for get_budget_by_id
+    
+    :param budget_id: Object ID of Budget Doc
+    :type budget_id: ObjectID
+    :return: Dictionary belonging to Budget_Id
+    :rtype: dict
+    """
+    return  db.budget.find_one(budget_id)
